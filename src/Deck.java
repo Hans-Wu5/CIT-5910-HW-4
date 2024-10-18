76c3f352fc12dcde6b240a34aea44552d544bc1c
hans <zihanw@seas.upenn.edu>
2024-10-17 22:57:46 -0400

    Generated full deck of cards

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,19 @@
 public class Deck {
 
+    //method for creating a deck of cards
+    public String[] deck() {
+        //setup deck array
+        String[] deck = new String[52];
+        String[] suits = {"♣", "♦", "♥", "♠"};
+        int counter = 0;
+
+        //nested for loops to merge suits with numbers
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 14; j++) {
+                deck[counter] = suits[i] + (j);
+                counter++;
+            }
+        }
+        return deck;
+    }
 }
